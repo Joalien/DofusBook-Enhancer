7a06514b82f829d2cfbcdf5b75cfec3bddd215bb
Josquin Cornec <josquin.cornec@kubys.fr>
2022-02-03 19:50:08 +0100

    sort 3 page of cloths

--- proxyResponse.js
+++ proxyResponse.js
@@ -1,30 +1,58 @@
 console.log("inside proxy response 2")
-let fullData = "";
+let fullData = {};
+let authHeader = [];
 
 function listener(details) {
-    console.log("inside listener 2 !");
+    // details.originUrl = "https://www.dofusbook.net/fr/encyclopedie/recherche?context=cloth&display=mosaic&sort=desc";
+    // details.documentUrl = "https://www.dofusbook.net/fr/encyclopedie/recherche?context=cloth&display=mosaic&sort=desc";
     let filter = browser.webRequest.filterResponseData(details.requestId);
     let decoder = new TextDecoder("utf-8");
     let encoder = new TextEncoder();
 
     filter.ondata = event => {
-        console.log("on data !");
         let data = decoder.decode(event.data, {stream: true});
         // Just change any instance of Example in the HTTP response
         // to WebExtension Example.
-        fullData += data
+        fullData[details.requestId] = fullData[details.requestId] === undefined ? data :  fullData[details.requestId] + data;
     }
 
     filter.onstop = event => {
-        console.log("on close");
-        let parsedData = JSON.parse(fullData);
-        parsedData.data = sortByResistance(parsedData);
+        let parsedData = JSON.parse(fullData[details.requestId]);
+        
+        // make more requests and mock backend sort
+        const params = new Proxy(new URLSearchParams(details.url), {
+            get: (searchParams, prop) => searchParams.get(prop),
+        });
+        if (params.page == 1) {
+            for (let i = 2; i <= 3; i++) {
+                let httpRequest = new XMLHttpRequest();
+                httpRequest.open("GET", details.url.replace('page=1', 'page=' + i), false);
+                authHeader.forEach(header => httpRequest.setRequestHeader(header.name, header.value))
+                httpRequest.send(null);
+                // get page i
+                console.log(httpRequest.response)
+                parsedData.data = parsedData.data.concat(JSON.parse(httpRequest.response).data);
+            }
+            parsedData.data = sortByResistance(parsedData);
+        }
         let stringData = JSON.stringify(parsedData);
         filter.write(encoder.encode(stringData));
         filter.close();
-        fullData = "";
+        fullData[details.requestId] = undefined;
     }
 }
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+    e => {
+        authHeader = e.requestHeaders;
+        console.log("saving header")
+        e.requestHeaders.push({name: "Referer", value: "https://www.dofusbook.net/fr/"})
+        return {requestHeaders: e.requestHeaders};
+    },
+    {urls: ["https://www.dofusbook.net/items/dofus/search/*", "https://www.dofusbook.net/api/cloths*"]},
+    ["blocking", "requestHeaders"]
+);
+
 
 browser.webRequest.onBeforeRequest.addListener(
     listener,
@@ -38,8 +66,6 @@ function sortByResistance(data) {
 
     sortedData.forEach(item => item.name += " (" + getSumOfRes(item) + "%)")
     sortedData.sort((a, b) => sortItems(a, b));
-    console.log('After sort, first item is ');
-    console.log(sortedData[0]);
     return sortedData;
 }
 
@@ -56,13 +82,24 @@ function getLevelOf(item) {
     return item.level;
 }
 
-function getSumOfRes(item) {
-    if (item.count_item != null) {
-       console.log("We are sorting cloths")
-       return Math.round(item.items.map(i => getSumOfRes(i)).reduce((a, b) => a + b, 0) / item.count_item);
-    }
+function getSumOfRes(clothOrItem) {
+    return clothOrItem.count_item != null ? getSumOfClothRes(clothOrItem) : getSumOfItemRes(clothOrItem);
+}
+
+function getSumOfClothRes(cloth) {
+    let sumOfItems = cloth.items.map(i => getSumOfRes(i)).reduce((a, b) => a + b, 0);
+    let sumOfClothEffect = getSumOfRes(mockItem(cloth));
+    return Math.round((sumOfItems + sumOfClothEffect) / cloth.count_item);
+}
+
+function getSumOfItemRes(item) {
     return item.effects
         .filter(effect => ["rnp", "rep", "rfp", "rap", "rtp",].includes(effect.name))
         .reduce((a, b) => parseInt(a) + parseInt(b.max), 0);
 }
 
+function mockItem(cloth) {
+    let fakeItem = {effects: cloth.cloth_effect};
+    fakeItem.effects.forEach(b => b.max = b.value)
+    return fakeItem;
+}
